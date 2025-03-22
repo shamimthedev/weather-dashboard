a30e7f7d9d1ee341a48ce054a0f30e949fb9fad1
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-22 22:21:57 +0600

    Updated ToggleUnits

--- src/components/ToggleUnits.jsx
+++ src/components/ToggleUnits.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const ToggleUnits = ({ toggleUnit, unit }) => {
   return (
-    <div className="unit-toggle">
-      <button onClick={toggleUnit} style={{ padding: '5px 10px', margin: '10px' }}>
+    <div>
+      <button
+        onClick={toggleUnit}
+        className="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md cursor-pointer transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      >
         Switch to {unit === 'metric' ? 'Fahrenheit' : 'Celsius'}
       </button>
     </div>
